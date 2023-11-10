b6f39079c879727c4ff8444f6dd3970425685bda
Paula Oseroff <oseroffp@gmail.com>
2023-11-10 03:09:20 -0300

    feat: add random shuffle picker

--- utils/kohonen/sample_pickers.py
+++ utils/kohonen/sample_pickers.py
@@ -5,3 +5,19 @@ def stochastic_picker(X: np.ndarray, memory: dict[str, Any]) -> tuple[np.ndarray
     p = X.shape[0]
     i = np.random.choice(p)
     return X[i]
+
+def random_shuffle_picker(X: np.ndarray, memory: dict[str, Any]) -> tuple[np.ndarray, dict[str, Any]]:
+    p = X.shape[0]
+
+    if 'I' not in memory or memory['i'] >= p:
+        memory['I'] = np.random.choice(p, size=p, replace=False)
+        memory['i'] = 0
+
+    I = memory['I']
+    i = memory['i']
+
+    aux = X[I[i]]
+
+    memory['i'] += 1
+
+    return aux, memory
