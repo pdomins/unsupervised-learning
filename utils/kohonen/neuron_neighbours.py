b31985d9e89311ab6b3e230ac01453586ed1f521
Paula Oseroff <oseroffp@gmail.com>
2023-11-10 01:46:45 -0300

    feat: add relu-like neighbour finder

--- utils/kohonen/neuron_neighbours.py
+++ utils/kohonen/neuron_neighbours.py
@@ -6,3 +6,19 @@ def radius_prop2iters_neighbours(curr_iter: int, iters: int, k: int, memory: dic
         memory['init_radius'] = math.sqrt(2)*k
 
     return (((1 - memory['init_radius']) / iters)*curr_iter + memory['init_radius'], memory)
+
+
+def relu_like_neighbours(curr_iter: int, iters: int, k: int, memory: dict[str, Any], alpha: float, beta: float) -> tuple[float, dict[str, Any]]:
+    if 'init_radius' not in memory:
+        memory['init_radius'] = math.sqrt(2)*k
+
+    if 'rect_iter_bound' not in memory:
+        memory['rect_iter_bound'] = iters - alpha
+
+    if curr_iter <= beta:
+        return memory['init_radius']
+
+    if curr_iter >= memory['rect_iter_bound']:
+        return 1
+
+    return (((1 - memory['init_radius']) / (iters - alpha - beta))*(curr_iter - beta) + memory['init_radius'], memory)
