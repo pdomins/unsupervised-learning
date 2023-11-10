77cf91fdfa201f91dd75174d5d35c5e683ab5070
Paula Oseroff <oseroffp@gmail.com>
2023-11-10 02:45:02 -0300

    feat: add flexible inverse proportional learning rate

--- utils/kohonen/neuron_lr.py
+++ utils/kohonen/neuron_lr.py
@@ -4,3 +4,6 @@ def fixed_learning_rate(curr_iter: int, lr: float = 0.1) -> float:
 
 def inv2iter_learning_rate(curr_iter: int) -> float:
     return 1 / (1 + curr_iter)
+
+def inv2iter_flex_learning_rate(curr_iter: int, a: float, b: float, c: float) -> float:
+    return a / (b + curr_iter) + c
