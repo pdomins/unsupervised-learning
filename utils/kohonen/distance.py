ba3f46b7727ca88d297a47036a43a857e128ae16
Paula Oseroff <oseroffp@gmail.com>
2023-11-11 01:57:18 -0300

    feat: add point inside radius function

--- utils/kohonen/distance.py
+++ utils/kohonen/distance.py
@@ -12,3 +12,12 @@ def euclidean_distance(X_1: np.ndarray, X_2: np.ndarray) -> float:
     sqr_norm = X_sqr_diff.sum().item()
 
     return math.sqrt(sqr_norm)
+
+def inside_radius(point: np.ndarray, center: np.ndarray, ry: float, rx: float) -> bool:
+    y_0 = center[0]
+    x_0 = center[1]
+
+    y = point[0]
+    x = point[1]
+
+    return ((x - x_0) / rx) ** 2 + ((y - y_0) / ry) ** 2 <= 1
