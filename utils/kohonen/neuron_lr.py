d4dcc1f8f568f867814d82488d45ae2311eefa93
Paula Oseroff <oseroffp@gmail.com>
2023-11-11 04:08:21 -0300

    feat: add default params for flex learning rate

--- utils/kohonen/neuron_lr.py
+++ utils/kohonen/neuron_lr.py
@@ -5,5 +5,5 @@ def fixed_learning_rate(curr_iter: int, lr: float = 0.1) -> float:
 def inv2iter_learning_rate(curr_iter: int) -> float:
     return 1 / (1 + curr_iter)
 
-def inv2iter_flex_learning_rate(curr_iter: int, a: float, b: float, c: float) -> float:
+def inv2iter_flex_learning_rate(curr_iter: int, a: float = 1, b: float = 1, c: float = 0) -> float:
     return a / (b + curr_iter) + c
