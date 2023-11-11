4a4b0135a54c8c4e4220dd29a672eb93214579de
Paula Oseroff <oseroffp@gmail.com>
2023-11-10 22:03:31 -0300

    style: rename neighbour functions to neighbour_radius

--- utils/kohonen/neuron_neighbours.py
+++ utils/kohonen/neuron_neighbours.py
@@ -1,13 +1,13 @@
 from typing import Any
 import math
 
-def radius_prop2iters_neighbours(curr_iter: int, iters: int, k: int, memory: dict[str, Any]) -> tuple[float, dict[str, Any]]:
+def prop2iters_neighbour_radius(curr_iter: int, iters: int, k: int, memory: dict[str, Any]) -> tuple[float, dict[str, Any]]:
     if 'init_radius' not in memory:
         memory['init_radius'] = math.sqrt(2)*k
 
     return (((1 - memory['init_radius']) / iters)*curr_iter + memory['init_radius'], memory)
 
-def relu_like_neighbours(curr_iter: int, iters: int, k: int, memory: dict[str, Any], alpha: float, beta: float) -> tuple[float, dict[str, Any]]:
+def relu_like_neighbour_radius(curr_iter: int, iters: int, k: int, memory: dict[str, Any], alpha: float, beta: float) -> tuple[float, dict[str, Any]]:
     if 'init_radius' not in memory:
         memory['init_radius'] = math.sqrt(2)*k
 
@@ -22,7 +22,7 @@ def relu_like_neighbours(curr_iter: int, iters: int, k: int, memory: dict[str, A
 
     return (((1 - memory['init_radius']) / (iters - alpha - beta))*(curr_iter - beta) + memory['init_radius'], memory)
 
-def exp_neighbours(curr_iter: int, iters: int, k: int, memory: dict[str, Any], gamma: float, delta: float) -> tuple[float, Any]:
+def exp_neighbour_radius(curr_iter: int, iters: int, k: int, memory: dict[str, Any], gamma: float, delta: float) -> tuple[float, Any]:
     return (delta*math.exp(-gamma*curr_iter) + 1, None)
 
 def exp_neighbour_influence_function(d: float, R: float) -> float:
