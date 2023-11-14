493a1ff6213c446e3d17868b80d9b35adb979933
Paula Oseroff <oseroffp@gmail.com>
2023-11-14 15:24:29 -0300

    feat(kohonen): add random state to neuron_weight_inits

--- utils/kohonen/neuron_weights.py
+++ utils/kohonen/neuron_weights.py
@@ -2,18 +2,23 @@ import numpy as np
 import math
 
 
-def random_weight_init(X: np.ndarray, k: int) -> np.ndarray:
+def random_weight_init(X: np.ndarray, k: int, 
+                       random_state: np.random.Generator = None) -> np.ndarray:
     n = X.shape[1]
 
     random_min = -1
     random_max = 1
     random_range = random_max - random_min
 
-    neuron_weights = np.random.random((k, k, n)) * random_range + random_min
+    if random_state is None:
+        random_state = np.random.default_rng()
+
+    neuron_weights = random_state.random((k, k, n)) * random_range + random_min
     return neuron_weights
 
 
-def random_sample_weight_init(X: np.ndarray, k: int, replace: bool) -> np.ndarray:
+def random_sample_weight_init(X: np.ndarray, k: int, replace: bool, 
+                              random_state: np.random.Generator = None) -> np.ndarray:
     p = X.shape[0]
     n = X.shape[1]
 
@@ -21,7 +26,10 @@ def random_sample_weight_init(X: np.ndarray, k: int, replace: bool) -> np.ndarra
 
     neuron_weights = np.zeros((k, k, n))
 
-    neuron_choice = np.random.choice(p, grid_size, replace)
+    if random_state is None:
+        random_state = np.random.default_rng()
+
+    neuron_choice = random_state.choice(p, grid_size, replace)
     neuron_choice = neuron_choice.reshape((k, k))
     for i in range(k):
         for j in range(k):
