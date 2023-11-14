fix(kohonen); better scaling for color map

utils/kohonen/plots.py
@@ -1,6 +1,7 @@
 from utils.kohonen.kohonen import KohonenNet
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
+import matplotlib.cm as cm
 import numpy as np
 
 def plot_u_mat(kohonen_net: KohonenNet, u_mat: np.ndarray, title: str = "Matriz U", format: str = "{:.2g}") -> None:
@@ -25,31 +26,18 @@ def plot_mat(kohonen_net: KohonenNet, mat: np.ndarray, title: str, format: str =
 
 def plot_neurons(kohonen_net: KohonenNet, x: np.ndarray, y: np.ndarray, 
                  w: np.ndarray, title: str, format: str = "{}") -> None:
-    cdict = {
-        'red':   ((0.0, 0.5, 0.5),
-                  (1.0, 1.0, 1.0)),
-
-        'green': ((0.0, 0.0, 0.0),
-                  (1.0, 1.0, 1.0)),
-            
-        'blue':  ((0.0, 0.5, 0.5),
-                  (1.0, 1.0, 1.0))
-    }
-
-    LavanderHaze = colors.LinearSegmentedColormap('LavanderHaze', cdict).reversed()
 
     min_w = w.min()
     max_w = w.max()
     range_w = max_w - min_w
-    c = ((w - min_w) / range_w) * 255
-    c = c.astype(int)
+    c = ((w - min_w) / range_w)
     
     ax = plt.gca()
     for i in range(kohonen_net.k*kohonen_net.k):
         circle = plt.Circle((x[i], y[i]), 0.45, edgecolor="k", 
-                            linewidth=1, facecolor=LavanderHaze(c[i]))
+                            linewidth=1, facecolor=cm.Purples(c[i]))
         ax.add_patch(circle)
-        if c[i] >= (2/3)*255:
+        if c[i] >= (2/3):
             ax.annotate(format.format(w[i]), xy=(x[i], y[i]), 
                         textcoords="data", ha="center", va="center", color="white")
         else:
@@ -62,7 +50,7 @@ def plot_neurons(kohonen_net: KohonenNet, x: np.ndarray, y: np.ndarray,
     ax.set_yticklabels([])
     ax.set_xticklabels([])
 
-    sm = plt.cm.ScalarMappable(cmap=LavanderHaze, 
+    sm = plt.cm.ScalarMappable(cmap=cm.Purples, 
                                norm=plt.Normalize(vmin=min_w, vmax=max_w))
     
     cb = plt.colorbar(mappable=sm, ax=ax)