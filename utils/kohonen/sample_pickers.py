7a1f5f7bc945d3f09e4d4e0e9d352f56f8bc0ddc
Paula Oseroff <oseroffp@gmail.com>
2023-11-13 22:18:05 -0300

    fix(kohonen): fix stochastic picker return values

--- utils/kohonen/sample_pickers.py
+++ utils/kohonen/sample_pickers.py
@@ -2,10 +2,10 @@ from typing import Any
 import numpy as np
 
 
-def stochastic_picker(X: np.ndarray, memory: dict[str, Any]) -> tuple[np.ndarray, Any]:
+def stochastic_picker(X: np.ndarray, memory: dict[str, Any]) -> tuple[np.ndarray, dict[str, Any]]:
     p = X.shape[0]
     i = np.random.choice(p)
-    return X[i]
+    return X[i], memory
 
 
 def random_shuffle_picker(X: np.ndarray, memory: dict[str, Any]) -> tuple[np.ndarray, dict[str, Any]]:
