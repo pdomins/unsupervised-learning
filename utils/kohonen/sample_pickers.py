49310555d0f63d26a4e3d788cd9937353192f331
Paula Oseroff <oseroffp@gmail.com>
2023-11-14 15:27:46 -0300

    feat(kohonen): add random state to pickers

--- utils/kohonen/sample_pickers.py
+++ utils/kohonen/sample_pickers.py
@@ -2,17 +2,26 @@ from typing import Any
 import numpy as np
 
 
-def stochastic_picker(X: np.ndarray, memory: dict[str, Any]) -> tuple[np.ndarray, dict[str, Any]]:
+def stochastic_picker(X: np.ndarray, memory: dict[str, Any], 
+                      random_state: np.random.Generator) -> tuple[np.ndarray, dict[str, Any]]:
+    if random_state is None:
+        random_state = np.random.default_rng()
+
     p = X.shape[0]
-    i = np.random.choice(p)
+    i = random_state.choice(p)
     return X[i], memory
 
 
-def random_shuffle_picker(X: np.ndarray, memory: dict[str, Any]) -> tuple[np.ndarray, dict[str, Any]]:
+def random_shuffle_picker(X: np.ndarray, memory: dict[str, Any], 
+                          random_state: np.random.Generator) -> tuple[np.ndarray, dict[str, Any]]:
     p = X.shape[0]
 
     if 'I' not in memory or memory['i'] >= p:
-        memory['I'] = np.random.choice(p, size=p, replace=False)
+        
+        if random_state is None:
+            random_state = np.random.default_rng()
+
+        memory['I'] = random_state.choice(p, size=p, replace=False)
         memory['i'] = 0
 
     I = memory['I']
