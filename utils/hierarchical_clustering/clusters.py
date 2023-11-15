551022086d6d5d5ebcd44c6053f32daf6d317620
Paula Domingues <pdomingues@itba.edu.ar>
2023-11-15 11:29:03 -0300

    feat: hclust conf matrix

--- utils/hierarchical_clustering/clusters.py
+++ utils/hierarchical_clustering/clusters.py
@@ -1,8 +1,11 @@
+import os
+import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
-from scipy.cluster.hierarchy import linkage, dendrogram
+from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 from scipy.spatial.distance import pdist, squareform
-
+from scipy.spatial import distance
 
 # def get_initial_matrix(df: pd.DataFrame):
 #     rows = df.shape[0]
@@ -17,6 +20,7 @@ from scipy.spatial.distance import pdist, squareform
 #             matrix[j, i] = distance
 #
 #     return matrix
+from sklearn.metrics import confusion_matrix
 
 
 def show_dendrogram(df: pd.DataFrame):
@@ -51,5 +55,30 @@ def get_initial_matrix(df: pd.DataFrame):
     return matrix
 
 
-def run(df: pd.DataFrame):
-    print("run hierarchical clustering")
+def plot_conf_matrix(text_df: pd.DataFrame):
+    to_predict = text_df['genres']
+    predictions = text_df['predicted_genre']
+    class_labels = text_df['genres'].unique()
+
+    cm = confusion_matrix(to_predict, predictions, labels=class_labels, normalize='true')
+
+    plt.figure(figsize=(8, 6))
+    plt.imshow(cm, interpolation='nearest', cmap=sns.cubehelix_palette(as_cmap=True, rot=.2, gamma=.5))
+    plt.title(f"Confusion Matrix for k = 3")
+    plt.colorbar()
+
+    tick_marks = np.arange(len(class_labels))
+    plt.xticks(tick_marks, class_labels, rotation=45)
+    plt.yticks(tick_marks, class_labels)
+
+    thresh = cm.max() / 2.
+    for i in range(len(class_labels)):
+        for j in range(len(class_labels)):
+            plt.text(j, i, "{:.2f}%".format(cm[i, j] * 100),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    os.makedirs("output", exist_ok=True)
+    plt.savefig(f"output/confusion_matrix_k3.png", bbox_inches='tight', dpi=1200)
+    plt.close()
